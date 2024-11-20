b50439c4c6268cfa6ee733aaa167c13398dc825a
philoloser <igawjanowska@gmail.com>
2024-11-20 13:28:56 +0100

    Edit

--- bits/str_join.py
+++ bits/str_join.py
@@ -1,3 +1,15 @@
 xs = ['1', '2', '3']
-s = ' '.join(xs)
-print(s)
+
+loop = 1
+looplist = []
+
+for i in xs:
+    lp = loop.__str__() + '. ' + i
+    print(lp)
+    looplist.append(loop)
+    loop = loop + 1
+
+print(looplist)
+
+# s = '_'.join(xs)
+# print(s)
